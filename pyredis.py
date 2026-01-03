60053c946f4eef7ccec7b4398eb5542b183e24b1
meashishjha <ashishgm98@gmail.com>
2026-01-03 15:24:43 +0530

    Adding python codes for accessing and managing Redis

---
+++ pyredis.py
@@ -0,0 +1,15 @@
+#############################################################################
+# (C) Copyright 2025 by Ashish Jha and Outshine Labs.                       #
+# All Rights Reserved.  https://wwww.ashishjha.com/                         #
+#                                                                           #
+# DISCLAIMER: The authors and publisher of this book/code have used their   #
+# best efforts in preparing the book/code. These efforts include the        #
+# development, research, and testing of the theories and programs           #
+# to determine their effectiveness. The authors and publisher make          #
+# no warranty of any kind, expressed or implied, with regard to these       #
+# programs or to the documentation contained in these books/codes.          #
+# The authors and publisher shall not be liable in any event for incidental #
+# or consequential damages in connection with, or arising out of, the       #
+# furnishing, performance, or use of these programs.                        #
+#############################################################################
+
